2e60c93e7327955f2f748f3dfd8ac89ae97848c7
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-07-18 21:12:42 -0400

    delete and post work. put passes as a freebie?

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const app = express();
-
-const logsController = require("./controllers/logsController")
+app.use(express.json()); // Important to be able to parse html data in as body
+const logsController = require("./controllers/logsController");
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Captain's Log!");
+  res.send("Welcome to Captain's Log!");
 });
-
-
 
 app.use("/logs", logsController);
 
-
-
-
-
-
-
 app.get("*", (req, res) => {
-    res.send("Error : Page not found");
-})
+  res.send("Error : Page not found");
+});
 
-module.exports = app
+module.exports = app;
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const logs = express.Router();
-const logsData = require("../models/log")
+const logsData = require("../models/log");
 
 logs.get("/", (req, res) => {
-    res.json(logsData);
+  res.json(logsData);
 });
+
 logs.get("/:id", (req, res) => {
-    console.log(req.params);
-    const logChoice = req.params
-    if (Number(logChoice.id) > logsData.length){
-        res.status(404).redirect('/error');
-    }
-    res.json(logsData[Number(logChoice.id)]);
+  console.log(req.params);
+  const logChoice = req.params;
+  if (Number(logChoice.id) > logsData.length) {
+    res.status(404).redirect("/error");
+  }
+  res.json(logsData[Number(logChoice.id)]);
+});
+
+logs.post("/", (req, res) => {
+  logsData.push(req.body);
+  res.json(logsData[logsData.length - 1]);
+});
+
+logs.delete("/:id", (req, res) => {
+    const index = req.param;
+    const flushedLog = logsData.splice(index, 1);
+    res.json(flushedLog);
 })
 
 module.exports = logs;
